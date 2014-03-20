dde2fb1e6ae7f5cd598507952c6fdf0aaed07ded
Raul RS <rrs2712@gmail.com>
2014-03-20 10:40:08 -0600

    Se agrego un test para probar commit and push de git plugin para eclipse
    Signed-off-by: Raul RS <rrs2712@gmail.com>

--- LaCuentaCore/src/test/java/com/tsis/lacuenta/core/bo/CtaPF_BOTest.java
+++ LaCuentaCore/src/test/java/com/tsis/lacuenta/core/bo/CtaPF_BOTest.java
@@ -3,9 +3,9 @@
  */
 package com.tsis.lacuenta.core.bo;
 
-import com.tsis.lacuenta.core.dto.Cta_DTO;
-
 import junit.framework.TestCase;
+
+import com.tsis.lacuenta.core.dto.Cta_DTO;
 
 /**
  * @author asus
@@ -50,12 +50,23 @@ public class CtaPF_BOTest extends TestCase {
 
 	/**
 	 * Test method for {@link com.tsis.lacuenta.core.bo.CtaPF_BO#getCtaIndividual_Redondeada()}.
+	 * 
+	 * Prueba 1
 	 */
-	public final void testGetCtaIndividual_Redondeada() {
-		
+	public final void testGetCtaIndividual_Redondeada1() {
 		CtaBO_IF cta = new CtaPF_BO(new Cta_DTO(475.58, 10, 50, true));
-		
 		assertTrue(0<cta.getCtaIndividual_Redondeada());
+//		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test method for {@link com.tsis.lacuenta.core.bo.CtaPF_BO#getCtaIndividual_Redondeada()}.
+	 * 
+	 * Prueba 2
+	 */
+	public final void testGetCtaIndividual_Redondeada2() {
+		CtaBO_IF cta = new CtaPF_BO(new Cta_DTO(475.58, 10, 50, true));
+		assertTrue(0>cta.getCtaIndividual_Redondeada());
 //		fail("Not yet implemented"); // TODO
 	}
 
